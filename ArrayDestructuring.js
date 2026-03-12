3c0e01710d57b2d10bf2a86a19fc3f2e82a19a69
zakirali003 <zakir.ali@invozone.dev>
2026-03-12 21:24:54 +0500

    modified the file ArrayDestructuring.js and put some extra use cases of Array destructuring

--- ArrayDestructuring.js
+++ ArrayDestructuring.js
@@ -20,3 +20,13 @@ let n = 30;
 console.log(`Before swap: m= ${m} and n= ${n}`);
 [m, n] = [n, m]; // swapping of value by array destructuring.
 console.log(`after swap m= ${m} and n= ${n}`);
+
+//Case 5: Loop me Destructuring
+let users = [['zakir', 30]];
+for ([Name, age] of users) {
+  console.log('name of user: ', Name, 'Age of user:', age);
+}
+//Case 6: Nested Array Destructuring...!
+let arrr = [1, 2, [3, 4, 5]];
+const [r1, r2, [r3, r4, r5]] = arrr;
+console.log('Nested Array destrc output: ', r1, r2, r3, r4, r5);
